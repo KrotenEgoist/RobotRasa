8df43222b7d93e321952c279fa5587564c757ee0
ElonMax <cappukan@gmail.com>
2023-03-22 19:00:38 +0300

    обновление генератора

--- src/generator/gen.py
+++ src/generator/gen.py
@@ -12,19 +12,6 @@ class Generator:
         self.morph = pymorphy2.MorphAnalyzer()
         self.key_pattern = re.compile(r'\w+:\w+')
 
-    @staticmethod
-    def _gen(func):
-        def wrapper(*args, **kwargs):
-            cmd_list = []
-            for sample in kwargs['samples']:
-                for i in range(kwargs['amount']):
-                    example = func(*args, sample, **kwargs)
-                    cmd_list.append(example)
-
-            return cmd_list
-
-        return wrapper
-
     def create_dictionary(self) -> dict:
         """
         Создает словарь из директории dictionary
@@ -50,7 +37,7 @@ class Generator:
 
         return dictionary
 
-    def inflect(self, word: str, grammes: set):
+    def inflect(self, words: list, grammes: set):
         """
         Преобразует слово в соответствии с граммемами (http://opencorpora.org/dict.php?act=gram)
 
@@ -59,11 +46,16 @@ class Generator:
         :return:
             Измененное слово: "идти", {'excl'} -> "иди"
         """
-        morphed = self.morph.parse(word)[0]
-        inflected = morphed.inflect(grammes)
-        new_word = inflected.word
+        morphed = []
+        for word in words:
+            print(word)
+            morph = self.morph.parse(word)[0]
+            inflected = morph.inflect(grammes)
+            new_word = inflected.word
 
-        return new_word
+            morphed.append(new_word)
+
+        return ' '.join(morphed)
 
     def create(self, sample: str):
         """
@@ -83,46 +75,79 @@ class Generator:
             измененная строка в соответствии с выбранными значениями словаря
 
         """
-        keys = self.key_pattern.findall(sample)
+        # keys = self.key_pattern.findall(sample)
+
+        keys = list(map(lambda x: x.split('-'), sample.split(" ")))
 
         case = None
-        words_dict = {}
-        for key in keys:
-            words = random.choice(self.dictionary[key]).split('|')
+        for subkey in keys:
+            words = None
+            inflected = []
+            for key in subkey:
+                words = random.choice(self.dictionary[key]).split('|')
 
-            if case:
-                try:
-                    word = self.inflect(words[0], case)
-                except AttributeError:
+                if case:
+                    word = self.inflect([words[0]], case)
+                else:
                     word = words[0]
+
+                inflected.append(word)
+
+            if len(inflected) > 1:
+                idx = list(map(lambda x: self.morph.parse(x)[0].tag.POS, inflected)).index('NOUN')
+                gender = self.morph.parse(inflected[idx])[0].tag.gender
+                # animacy = self.morph.parse(inflected[idx])[0].tag.animacy
+
+                inflected = self.inflect(inflected, {gender})
             else:
-                word = words[0]
+                inflected = inflected[0]
 
             if len(words) > 1:
                 case = set(words[1:])
             else:
                 case = None
 
-            words_dict[key] = word
+            print(inflected)
 
-        for key, value in words_dict.items():
-            sample = sample.replace(key, value)
+        # case = None
+        # words_dict = {}
+        # for key in keys:
+        #     words = random.choice(self.dictionary[key]).split('|')
+        #
+        #     if case:
+        #         try:
+        #             word = self.inflect(words[0], case)
+        #         except AttributeError:
+        #             word = words[0]
+        #     else:
+        #         word = words[0]
+        #
+        #     if len(words) > 1:
+        #         case = set(words[1:])
+        #     else:
+        #         case = None
+        #
+        #     words_dict[key] = word
+        #
+        # for key, value in words_dict.items():
+        #     sample = sample.replace(key, value)
+        #
+        # return sample.strip()
 
-        return sample.strip()
+    def run(self, samples, amount=100, start=None, end=None):
 
-    @_gen
-    def commands_with_numbers(self, sample, **kwargs):
-        example = self.create(sample)
-        n = random.randint(kwargs['start'], kwargs['end'])
-        example = example.format(n)
+        cmd_list = []
+        for sample in samples:
+            for i in range(amount):
+                example = self.create(sample)
 
-        return example
+                if start and end:
+                    n = random.randint(start, end)
+                    example = example.format(n)
 
-    @_gen
-    def commands_without_numbers(self, sample, **kwargs):
-        example = self.create(sample)
+                cmd_list.append(example)
 
-        return example
+        return cmd_list
 
 
 if __name__ == '__main__':
@@ -130,40 +155,9 @@ if __name__ == '__main__':
     dict_path = project_path.joinpath("src/generator/dictionary")
     generator = Generator(dict_path)
 
-    # patrols = [
-    #     "prep:robot action:patrol",
-    #     "prep:robot action:patrol aux:by object:circle {} distance:meter",
-    #     "prep:robot action:patrol aux:by object:circle aux:radius {} distance:meter",
-    #     "prep:robot action:patrol aux:by object:route {}",
-    #     "prep:robot action:patrol aux:by {} object:route",
-    #     "prep:robot action:patrol aux:by object:route aux:number {}",
-    # ]
-    # action_patrol = generator.commands_with_numbers(samples=patrols, amount=10, start=1, end=10)
-    # for act in action_patrol:
-    #     print(act)
-    #
-    # stops = [
-    #     "prep:robot action:stop",
-    # ]
-    # action_stop = generator.commands_without(samples=stops, amount=10)
-    # for act in action_stop:
-    #     print(act)
-    #
-    # directions = [
-    #     "prep:robot action:move direction:forward {} distance:meter",
-    #     "prep:robot action:move {} distance:meter direction:forward"
-    # ]
-    # action_move_direction = generator.commands_with_numbers(samples=directions, amount=10, start=1, end=50)
-    # for act in action_move_direction:
-    #     print(act)
-
-    samples = [
-        "prep:robot action:move aux:to object:tree relation:behind object:human",
-        "prep:robot action:around object:human",
-        "prep:robot action:move aux:to feature:nearest object:human"
+    exmp = [
+        "prep:robot action:move aux:to feature:nearest-object:tree"
     ]
 
-    actions = generator.commands_without_numbers(samples=samples, amount=100)
+    a = generator.run(samples=exmp)
 
-    for act in actions:
-        print(act)
